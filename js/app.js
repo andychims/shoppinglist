6f72724c475bae632b8d1fc8a267250bff1f22e7
andysplice <andy@splice.com>
2014-12-22 10:30:23 -0500

    pretty much done

--- js/app.js
+++ js/app.js
@@ -4,29 +4,37 @@ $(document).ready(function() {
  function submitItems() {
  		//set todo_text variable to whatever's in the text input form
 		var todo_text = $(".add_form").val();
+		var price = $(".price_form").val();
+		var quantity = $(".quantity_form").val();
 		//append to the ul, "<li>"+todo_text+"</li>"
-		$(".add_button").closest("#main").find("ul").append("<li>" + todo_text + "</li>");
-		//on pressing add, clear the form contents
-		$(".add_form").val("");
+		if(!$.trim($('.add_form').val())) {
+			alert('Please enter text to add to the list');
+		} 
+		else {
+			$(".add_button").closest("#main").find("ul").append("<li>" + todo_text + " = $" + price*quantity + "</li>");
+			//on pressing add, clear the form contents
+			$(".add_form").val("");
+			$(".price_form").val("");
+			$(".quantity_form").val("");
+		};
   };
 
 	//when i press the submit button, then do a function
 	$("form").on("click", ".add_button", submitItems);
 
 
+//set variable 'price' for value in class=price_form
+//append price to li after submitting
 
 
-//use enter key to submit
+	//use enter key to submit
 	$(document).keypress(function(e) {
 	    if(e.which == 13) {
 		    // submitItems;
-		    e.preventDefault();
 		    submitItems();
-		    
+		    e.preventDefault();
 		}
 	});
-
-
 
 
 	//on clicking the li, cross out the text using class=completed
@@ -43,29 +51,32 @@ $(document).ready(function() {
 		// find li with completed class, delete them
 
 
+//sortable list items
+	// $('ul').sortable({ axis: "y" });
 
-//*** this isn't working on newly made li's!
-//on hover of li, append " - mark completed" inside of the li
+    $( "ul" ).sortable();
+
+	//on hover of li, append " - mark completed" inside of the li
 	//if it's not already checked - ie doesn't have the completed class
 	//on mouseover add li_hover class, on mouseout remove the li_hover class
-		$("ul").on("mouseover", "li", function(event){
-			$(this).addClass("li_hover");
+	$("ul").on("mouseenter", "li", function(){
+		if ($(this).hasClass("completed")) {	
+		}
+		else {
+			$(this).toggleClass("li_hover");
 			$(this).append("<span class='mark_completed'>&nbsp; - &nbsp;(mark completed)</span>");
-		});
-		$("ul").on("mouseout", "li", function(event){
-			$(this).find("span").remove();
-		});
-	//if it does have the completed class, don't do anything
+			$(this).prepend("<i class='fa fa-navicon hamburger'></i>");
+		}
+	});
+	$("ul").on("mouseleave", "li", function(){
+		$(this).find("span").remove();
+		$(this).removeClass("li_hover");
+		$(this).find("i").remove();
+	});
 
 
-//trying out hover, but this doesn't work on newly-created li's
-// $( "li" ).hover(
-//   function() {
-//     $( this ).append( $( "<span class='mark_completed'>&nbsp; - &nbsp;(mark completed)</span>" ) );
-//   }, function() {
-//     $( this ).find( "span:last" ).remove();
-//   }
-// );
+
+
 
 
 });
